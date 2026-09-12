cf2de6291b14456de83c6ba432359cd7a3538626
sjmace-sys <sjmace@gmail.com>
2026-09-12 15:02:27 +0100

    Fix TJ logo PNGs and refresh app icon cache

--- sw.js
+++ sw.js
@@ -1,29 +1,41 @@
 const CACHE='tj-finder-v4';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png?v=4','./icon-512.png?v=4'];
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache=>cache.addAll(ASSETS.map(url=>new Request(url,{cache:'reload'}))))
+      .then(()=>self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
   if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
-      return response;
-    }).catch(()=>caches.match('./index.html')));
+    event.respondWith(
+      fetch(event.request).then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
+        return response;
+      }).catch(()=>caches.match('./index.html'))
+    );
     return;
   }
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
-    if(new URL(event.request.url).origin===self.location.origin){
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-    }
-    return response;
-  })));
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+      if(new URL(event.request.url).origin===self.location.origin){
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      }
+      return response;
+    }))
+  );
 });
