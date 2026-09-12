482f2ae913b2a64adc8bdd4ec9c191f76bc2b661
sjmace-sys <sjmace@gmail.com>
2026-09-12 14:56:47 +0100

    Refresh app icon cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='tj-finder-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='tj-finder-v4';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png?v=4','./icon-512.png?v=4'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
